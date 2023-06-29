5609cc2270d1af88f3704c0595286110da978989
Martin Luther Aguila <martin.luther.aguila@gmail.com>
2023-06-29 21:57:12 +0800

    initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,44 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  items: any[] = [];
+  totalAmount: number = 0;
+
   constructor() {}
+
+  addItem() {
+    this.items.push({ name: '', price: 0, quantity: 1 });
+    this.updateTotal();
+  }
+
+  deleteItem(index: number) {
+    this.items.splice(index, 1);
+    this.updateTotal();
+  }
+
+  updateTotal() {
+    this.totalAmount = this.items.reduce(
+      (total, item) => total + item.price * item.quantity,
+       0
+    );
+  }
+
+  clearPrice(item: any) {
+    if (item.price === 0) {
+      item.price = null;
+    }
+  }
+
+  clearQuantity(item: any) {
+    if (item.quantity === 1) {
+      item.quantity = null;
+    }
+  }
+
+  resetPrice(item: any) {
+    if (!item.price) {
+      item.price = 0;
+    }
+  }
 
 }
